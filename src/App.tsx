4fb2552200a5dcca95778c77df9602b1af82d06e
brett.cole <acodercalledcole@gmail.com>
2024-04-02 11:48:02 -0400

    homepage layout and structure w/meta data included

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import { lazy, Suspense, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import Navigation from "./routes/navigation/navigation.component";
-import Home from "./routes/home/home.component";
+import Spinner from "./components/spinner/spinner.component";
+import { useDispatch } from "react-redux";
+import { checkUserSession } from "./store/user/user.action";
+
+const Navigation = lazy(() => import("./routes/navigation/navigation.component"));
+const Home = lazy(() => import("./routes/home/home.component"));
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import ("./routes/checkout/checkout.component"));
+const Authentication = lazy(() =>
+	import("./routes/authentication/authentication.component")
+);
 
 const App = () => {
+	const dispatch = useDispatch();
+	
+	useEffect(() => {
+		dispatch(checkUserSession());
+	}, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+					<Route path="auth" element={<Authentication />} />
+					<Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+import stripePromise from "./utils/stripe/stripe.utils.js";
+import App from "./App.tsx"
+import { store, persistor } from "./store/store";
+
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+					<Elements stripe={stripePromise}>
+            <App />
+					</Elements>
+        </BrowserRouter>
+			</PersistGate>
+		</Provider>
   </React.StrictMode>,
 )
