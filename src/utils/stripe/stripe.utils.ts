195990709ab8ac5aa1a975684f4ccabfd1e208c8
brett.cole <acodercalledcole@gmail.com>
2024-04-15 22:46:55 -0400

    stripe and firebase util functions

---
+++ src/utils/stripe/stripe.utils.ts
@@ -0,0 +1,7 @@
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+
+const stripePromise: Promise<Stripe | null> = loadStripe(
+  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
+);
+
+export default stripePromise;
