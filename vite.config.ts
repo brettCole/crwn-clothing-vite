1b09c68e0259722ec51be705e7962c956c7a1a9c
brett.cole <acodercalledcole@gmail.com>
2024-04-15 22:49:59 -0400

    vite configuration for esm and plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
+import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), svgr()],
+// })
+
 export default defineConfig({
-  plugins: [react(), svgr()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        format: 'esm', // Change to ESM format
+      }
+    }
+  },
+  plugins: [
+    svgrPlugin(),
+    react()
+  ]
 })
